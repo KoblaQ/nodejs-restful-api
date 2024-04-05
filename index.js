dab03e4210bd2f1f4017559ae832dceebcc9b6ab
Edem <edemq123@gmail.com>
2024-04-05 11:24:34 +0300

    Added static files folder and css

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ app.engine(
 
 app.set("view engine", "handlebars"); // Use handlebars as a template engine
 
+// Render the homepage to the browser
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+// Set the folder for static files (css, jpg)
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // Confirmation of server running successfully
